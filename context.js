2a731abe676a6663f965317b13bb10eadd414376
sudke1234 <129704332+sudke1234@users.noreply.github.com>
2023-12-04 04:45:58 +0900

    수정3

--- context.js
+++ context.js
@@ -1,7 +1,7 @@
 function showContent() {
     const p1 = document.getElementById('problem1');
     const content = "소각<br>심각한 대기오염의 원인"; //가장 많이 쓰이는 쓰레기 처리 방법이다.
-    p1.style.color = "white";
+    p1.style.color = "black";
     p1.style.fontSize = "25px"; 
     p1.innerHTML = content;
   }
@@ -15,7 +15,7 @@ function showContent() {
   function showContent2() {
     const p1 = document.getElementById('problem2');
     const content = "매립<br>한정적인 매립지 수"; //매립지 밑에 묻어진 플라스틱에서 독성 물질이 나와<br>지하수로 스며든 후 하류로 흘러가 호수와 강으로까지 흘러가게 된다.
-    p1.style.color = "white";
+    p1.style.color = "black";
     p1.style.fontSize = "25px"; 
     p1.innerHTML = content;
   }
@@ -31,7 +31,7 @@ function showContent() {
   function showContent3() {
     const p1 = document.getElementById('problem3');
     const content = "해양<br>해양 생태계를 파괴"; //생물이 해양 플라스틱 쓰레기를 먹이로 착각하여 먹는 바람에<br>위나 장에 쌓여 죽음에 이르는 사례가 보고되고 있다
-    p1.style.color = "white";
+    p1.style.color = "black";
     p1.style.fontSize = "25px"; 
     p1.innerHTML = content;
   }
@@ -48,7 +48,7 @@ function showContent() {
   function showContent4() {
     const p1 = document.getElementById('problem4');
     const content = "재활용<br>매우 낮은 재활용률"; //특정 물건을 다시 사용한다는 뜻을 가지고 있다.<br>친환경적인 방법이다.
-    p1.style.color = "white";
+    p1.style.color = "black";
     p1.style.fontSize = "25px"; 
     p1.innerHTML = content;
   }
